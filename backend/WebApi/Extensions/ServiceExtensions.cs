3f3487e901541b42a795f8c45519cb682c6fd3c6
Eduardo Verbinen <everbinen@gmail.com>
2025-10-18 22:58:03 -0300

    feat: adds db configuration

---
+++ backend/WebApi/Extensions/ServiceExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.Handlers;
+
+namespace WebApi.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static IConfigurationRoot ConfigureAppSettings(this WebApplicationBuilder builder)
+        {
+            return builder.Configuration
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: false, reloadOnChange: true)
+                .Build();
+        }
+
+        public static IServiceCollection ConfigureDatabase(this WebApplicationBuilder builder)
+        {
+            return builder.Services.AddDbContext<FactorySystemsDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DatabaseConnection")));
+        }
+    }
+}
--- backend/WebApi/Program.cs
+++ backend/WebApi/Program.cs
@@ -1,3 +1,4 @@
+using WebApi.Extensions;
 
 namespace FactorySystems
 {
@@ -7,16 +8,15 @@ namespace FactorySystems
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            builder.ConfigureAppSettings();     
+            builder.ConfigureDatabase();
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
