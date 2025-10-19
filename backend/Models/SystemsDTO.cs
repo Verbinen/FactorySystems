13de6ad390e51f1fe78cb07d451c7fa2fd88c4b4
Eduardo Verbinen <everbinen@gmail.com>
2025-10-18 22:48:21 -0300

    feat: renames FactoryData to SystemsDTO

---
+++ backend/Models/SystemsDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Models
+{
+    public sealed class SystemsDTO
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string ApplicationName { get; set; }
+        public string ApplicationCode { get; set; }
+        public string CostCenter { get; set; }
+        public string[] EmailSupport { get; set; }
+        public string Status { get; set; }
+        public string Database { get; set; }
+        public string InstallationLocation { get; set; }
+    }
+}
