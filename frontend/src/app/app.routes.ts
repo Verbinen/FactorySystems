9bc82e3a6b6089c9fad0c4a29c89779879edbdeb
Eduardo Verbinen <everbinen@gmail.com>
2025-10-20 21:53:11 -0300

    feat: add register component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,5 +4,9 @@ export const routes: Routes = [
   {
     path: "",
     loadComponent: () => import("../app/features/system-list/system-list").then(c => c.SystemList)
+  },
+    {
+    path: "register",
+    loadComponent: () => import("../app/features/system-register/system-register").then(c => c.SystemRegister)
   }
 ];
---
+++ frontend/src/app/features/system-register/system-register.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-system-register',
+  imports: [],
+  templateUrl: './system-register.html',
+  styleUrl: './system-register.scss'
+})
+export class SystemRegister {
+
+}
