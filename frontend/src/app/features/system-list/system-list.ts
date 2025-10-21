4cca3d7bc4140a4f25356d310e329c43c4af844a
Eduardo Verbinen <everbinen@gmail.com>
2025-10-20 21:52:43 -0300

    feat: add system list component

---
+++ frontend/src/app/features/system-list/system-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-system-list',
+  imports: [],
+  templateUrl: './system-list.html',
+  styleUrl: './system-list.scss'
+})
+export class SystemList {
+
+}
