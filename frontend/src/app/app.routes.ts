f6ed8f2fe84b854aa5c93f3c5897b6e5509ef0a0
Eduardo Verbinen <everbinen@gmail.com>
2025-10-22 00:29:09 -0300

    feat: adds app route & improve index

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,8 +5,12 @@ export const routes: Routes = [
     path: "",
     loadComponent: () => import("../app/features/system-list/system-list").then(c => c.SystemList)
   },
-    {
+  {
     path: "register",
+    loadComponent: () => import("../app/features/system-register/system-register").then(c => c.SystemRegister)
+  },
+  {
+    path: "edit/:id",
     loadComponent: () => import("../app/features/system-register/system-register").then(c => c.SystemRegister)
   }
 ];
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/components/header/header';
+import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, ToastModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
